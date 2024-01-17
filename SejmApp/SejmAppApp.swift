14f2ce25d73a10c4e6e2cd967fecc17ce9491d86
Dawid Packowski <dawid@packowski.net>
2024-01-17 20:48:34 +0100

    Initial Commit

---
+++ SejmApp/SejmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SejmAppApp.swift
+//  SejmApp
+//
+//  Created by Dawid Paćkowski on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SejmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
