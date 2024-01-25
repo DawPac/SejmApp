270275e8205eadaf03eed0b8988922a93d276e64
Dawid Packowski <dawid@packowski.net>
2024-01-25 17:01:55 +0100

    App version 1.1.2

---
+++ SejmApp/Live.swift
@@ -0,0 +1,8 @@
+//
+//  Live.swift
+//  SejmApp
+//
+//  Created by Dawid Paćkowski on 25/01/2024.
+//
+
+import Foundation
---
+++ SejmApp/LiveView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveView.swift
+//  SejmApp
+//
+//  Created by Dawid Paćkowski on 25/01/2024.
+//
+
+import SwiftUI
+
+struct LiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveView()
+}
